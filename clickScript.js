b97d1169221ae790359b19484e52780db4fbef60
lloydiecarr <lloydiecarr@gmail.com>
2016-04-27 12:27:36 +0100

    Added files via upload

--- clickScript.js
+++ clickScript.js
@@ -5,18 +5,22 @@ $(document).ready(function(){
 		var chooseArray = [];
 		var clicks = 0;
 		
-	$("#card").text(items[0]);
+	$("#card").text('"' + items[0] + '"');
+	$("#count").text(clicks + 1);
 	
 	$("#btn-yes").click(function(){
 		clicks++;
 		var chosen = Math.floor(clicks - 1);
+		var next = items[clicks];
 		
+			$("#card").text('"' + next + '"');
+			
 		if (clicks <= items.length - 1){
 			var result = items[chosen];
-				$("#card").text(result);
+				$("#count").text(clicks + 1);
 				$("#chosen").append('<p>' + result + '</p><hr />');
 					} else {
-						$("#card").text("You are finished");
+				$("#card").text("You are finished");
 			}
 		});
 		
@@ -25,9 +29,10 @@ $(document).ready(function(){
 		
 		if (clicks <= items.length - 1){
 			var result = items[clicks];
-				$("#card").text(result);
+				$("#count").text(clicks + 1);
+				$("#card").text('"' + result + '"');
 					} else {
-						$("#card").text("You are finished");
+				$("#card").text("You are finished");
 			}
 		});
 	});
