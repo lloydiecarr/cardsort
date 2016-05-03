9a62e50cde4c98929f565c7cbafe96a6f6b2daa1
lloydiecarr <lloydiecarr@gmail.com>
2016-05-03 18:12:49 +0100

    Added files via upload

--- clickScript.js
+++ clickScript.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+	
 	//import word list
 	$.get("textFile.txt", function(data) {
 		//split data at new lines
@@ -15,7 +16,6 @@ $(document).ready(function(){
 	//detect click
 	$("#btn-yes").click(function(){
 		clicks++;
-		
 		//minus 1 to record last clicked item not next
 		var chosen = Math.floor(clicks - 1);
 		var next = items[clicks];
@@ -25,13 +25,20 @@ $(document).ready(function(){
 		
 		//update chosen list & on screen counter
 		if (clicks <= items.length - 1){
+		
 			var result = items[chosen];
 				$("#count").text(clicks);
-				$("#chosen").append('<p>' + result + '</p><hr />');
+				$("#chosen").append('<p id='+clicks+'>' + result + '</p><hr />');
 					} else {
+					
 				//if there are no more words
 				$("#card").text("You are finished");
 			}
+					//to highlight from selected list
+					$('#'+clicks).click(function(){
+						$(this).css('font-size','2em');
+					});
+			
 		});
 		
 	$("#btn-no").click(function(){
@@ -46,5 +53,7 @@ $(document).ready(function(){
 			}
 		});
 	});
+	
+
 		
 }); 
